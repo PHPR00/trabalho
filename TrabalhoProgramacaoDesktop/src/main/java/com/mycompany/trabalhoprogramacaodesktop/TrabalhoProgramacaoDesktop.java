b0d7a8206ad5bc645d6440b9cfa6ef2c2e0f06f0
Aluno UNIPAR <aluno@ac-cas.unipar.br>
2024-03-27 21:55:24 -0300

    Trabalho

---
+++ TrabalhoProgramacaoDesktop/src/main/java/com/mycompany/trabalhoprogramacaodesktop/TrabalhoProgramacaoDesktop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhoprogramacaodesktop;
+
+/**
+ *
+ * @author aluno
+ */
+public class TrabalhoProgramacaoDesktop {
+
+    public static void main(String[] args) {
+        new FrameTelaLogin().setVisible(true);
+    }
+}
